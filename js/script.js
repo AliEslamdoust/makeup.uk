2e288fe86ef09f168320facb6de3e10ef293f378
aliesi3410 <52535627+aliesi3410@users.noreply.github.com>
2023-11-25 17:44:29 +0330

    full header

--- js/script.js
+++ js/script.js
@@ -62,3 +62,148 @@ showPass.addEventListener("click", function () {
   this.classList.toggle("fa-eye");
   passInput.type = passInput.type == "password" ? "text" : "password";
 });
+
+// sub sub menu activator
+let ssMenuActivators = document.querySelectorAll(".ssMenuActive");
+let ssMenu = document.querySelectorAll(".ssMenu");
+let ssMenuHeight;
+let currentssMenu;
+
+ssMenuActivators.forEach((elem, index) => {
+  elem.addEventListener("click", function () {
+    ssMenu[index].classList.toggle("isActive");
+    let liItemsCount = ssMenu[index].childElementCount;
+    ssMenuHeight = liItemsCount * 28;
+    currentssMenu = index;
+
+    if (ssMenu[index].classList.contains("isActive")) {
+      currentHeight = 0;
+    } else {
+      currentHeight = ssMenu[index].getBoundingClientRect().height / 10;
+    }
+
+    callSMenu();
+
+    if (ssMenuActivators[index].classList.contains("active")) {
+      ssMenuActivators[index].classList.remove("active");
+    } else {
+      ssMenuActivators[index].classList.add("active");
+    }
+  });
+});
+
+function callSMenu() {
+  openSSMenu(ssMenuHeight, currentssMenu);
+}
+
+let currentHeight = 0;
+function openSSMenu(height, index) {
+  if (ssMenu[index].classList.contains("isActive")) {
+    if (currentHeight * 10 < height) {
+      ssMenu[index].style.height = currentHeight * 10 + "px";
+      currentHeight++;
+      setTimeout(callSMenu, 10);
+    } else {
+      ssMenu[index].removeAttribute("style");
+      ssMenu[index].classList.add("active");
+    }
+  } else {
+    ssMenu[index].classList.remove("active");
+    if (currentHeight * 10 > 0) {
+      ssMenu[index].style.height = currentHeight * 10 + "px";
+      currentHeight--;
+      setTimeout(callSMenu, 10);
+    } else {
+      ssMenu[index].removeAttribute("style");
+    }
+  }
+}
+
+// slider with opacity
+
+let sliderContainer = document.getElementById("sliderContainer");
+let sliders = document.querySelectorAll(".slider");
+
+// pagination
+let paginationContainer = document.getElementById("sliderPagination");
+
+for (let i = 0; i < sliders.length; i++) {
+  paginationContainer.appendChild(document.createElement("span"));
+}
+
+let paginationBtn = document.querySelectorAll("#sliderPagination>span");
+function changePagination() {
+  clearPagination();
+  paginationBtn[currentSlide].classList.add("active");
+}
+
+// move slider
+let currentSlide = 0;
+let sliderDelay = 5000;
+
+function callSlider() {
+  sliderTimeout = setTimeout(moveSlider, sliderDelay);
+}
+
+function clearSliderTimeout() {
+  clearTimeout(sliderTimeout);
+}
+
+function moveSlider() {
+  if (currentSlide < sliders.length) {
+    changePagination();
+    clearSliders();
+    sliders[currentSlide].classList.add("active");
+    currentSlide++;
+    callSlider();
+  } else {
+    currentSlide = 0;
+    moveSlider();
+  }
+}
+
+function clearSliders() {
+  sliders.forEach((elem) => elem.classList.remove("active"));
+}
+
+moveSlider();
+
+// navigation slider
+let nextBtn = document.getElementById("sliderNext");
+let prevBtn = document.getElementById("sliderPrev");
+
+function prevSlide() {
+  clearSliderTimeout();
+  if (currentSlide > 1) {
+    currentSlide -= 2;
+  } else {
+    currentSlide = sliders.length - 1;
+  }
+  moveSlider();
+}
+
+function nextSlide() {
+  clearSliderTimeout();
+  if (currentSlide == sliders.length) {
+    currentSlide = 0;
+  }
+  moveSlider();
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+// move with pagination
+paginationBtn.forEach((elem, index) => {
+  elem.addEventListener("click", function () {
+    clearPagination();
+    this.classList.add("active");
+    currentSlide = index;
+    clearSliderTimeout();
+    moveSlider();
+  });
+});
+
+function clearPagination() {
+  paginationBtn.forEach((elem) => elem.classList.remove("active"));
+}
