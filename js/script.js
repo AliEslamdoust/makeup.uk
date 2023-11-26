54072aa20892dfc0ed466a713ccda8dc69a171b2
aliesi3410 <52535627+aliesi3410@users.noreply.github.com>
2023-11-27 01:40:12 +0330

    full responsive

--- js/script.js
+++ js/script.js
@@ -31,6 +31,13 @@ let loginContainer = document.getElementById("loginContainer");
 let loginInputs = document.querySelectorAll(".loginInput");
 let loginLabels = document.querySelectorAll(".loginLabel");
 let loginInputContainers = document.querySelectorAll(".inputContainer");
+let loginCloseBtn = document.getElementById("loginCloseBtn");
+
+loginCloseBtn.addEventListener("click", () => {
+  loginContainer.classList.toggle("show");
+  loginFallBack.classList.toggle("hide");
+});
+
 loginBtn.addEventListener("click", function () {
   loginContainer.classList.toggle("show");
   loginFallBack.classList.toggle("hide");
@@ -63,60 +70,80 @@ showPass.addEventListener("click", function () {
   passInput.type = passInput.type == "password" ? "text" : "password";
 });
 
-// sub sub menu activator
+// sub sub menu activator in desktop
 let ssMenuActivators = document.querySelectorAll(".ssMenuActive");
 let ssMenu = document.querySelectorAll(".ssMenu");
 let ssMenuHeight;
 let currentssMenu;
+if (window.innerWidth > 1024) {
+  ssMenuActivators.forEach((elem, index) => {
+    elem.addEventListener("click", function () {
+      ssMenu[index].classList.toggle("isActive");
+      let liItemsCount = ssMenu[index].childElementCount;
+      ssMenuHeight = liItemsCount * 28;
+      currentssMenu = index;
 
-ssMenuActivators.forEach((elem, index) => {
-  elem.addEventListener("click", function () {
-    ssMenu[index].classList.toggle("isActive");
-    let liItemsCount = ssMenu[index].childElementCount;
-    ssMenuHeight = liItemsCount * 28;
-    currentssMenu = index;
+      if (ssMenu[index].classList.contains("isActive")) {
+        currentHeight = 0;
+      } else {
+        currentHeight = ssMenu[index].getBoundingClientRect().height / 10;
+      }
 
-    if (ssMenu[index].classList.contains("isActive")) {
-      currentHeight = 0;
-    } else {
-      currentHeight = ssMenu[index].getBoundingClientRect().height / 10;
-    }
+      callSMenu();
 
-    callSMenu();
-
-    if (ssMenuActivators[index].classList.contains("active")) {
-      ssMenuActivators[index].classList.remove("active");
-    } else {
-      ssMenuActivators[index].classList.add("active");
-    }
+      if (ssMenuActivators[index].classList.contains("active")) {
+        ssMenuActivators[index].classList.remove("active");
+      } else {
+        ssMenuActivators[index].classList.add("active");
+      }
+    });
   });
-});
 
-function callSMenu() {
-  openSSMenu(ssMenuHeight, currentssMenu);
-}
+  function callSMenu() {
+    openSSMenu(ssMenuHeight, currentssMenu);
+  }
 
-let currentHeight = 0;
-function openSSMenu(height, index) {
-  if (ssMenu[index].classList.contains("isActive")) {
-    if (currentHeight * 10 < height) {
-      ssMenu[index].style.height = currentHeight * 10 + "px";
-      currentHeight++;
-      setTimeout(callSMenu, 10);
+  let currentHeight = 0;
+  function openSSMenu(height, index) {
+    if (ssMenu[index].classList.contains("isActive")) {
+      if (currentHeight * 10 < height) {
+        ssMenu[index].style.height = currentHeight * 10 + "px";
+        currentHeight++;
+        setTimeout(callSMenu, 10);
+      } else {
+        ssMenu[index].removeAttribute("style");
+        ssMenu[index].classList.add("active");
+      }
     } else {
-      ssMenu[index].removeAttribute("style");
-      ssMenu[index].classList.add("active");
-    }
-  } else {
-    ssMenu[index].classList.remove("active");
-    if (currentHeight * 10 > 0) {
-      ssMenu[index].style.height = currentHeight * 10 + "px";
-      currentHeight--;
-      setTimeout(callSMenu, 10);
-    } else {
-      ssMenu[index].removeAttribute("style");
+      ssMenu[index].classList.remove("active");
+      if (currentHeight * 10 > 0) {
+        ssMenu[index].style.height = currentHeight * 10 + "px";
+        currentHeight--;
+        setTimeout(callSMenu, 10);
+      } else {
+        ssMenu[index].removeAttribute("style");
+      }
     }
   }
+} else {
+  console.log("no");
+  let menuBtn = document.querySelectorAll(".hB-Menu>li");
+  let subMenu = document.querySelectorAll(".submenu");
+  let menuChevron = document.querySelectorAll(".menuChevron");
+  let headerBottom = document.getElementById("headerBottom");
+  let mobileMenu = document.getElementById("mobileMenu");
+
+  mobileMenu.addEventListener("click", function () {
+    this.classList.toggle("active");
+    headerBottom.classList.toggle("active");
+  });
+
+  menuBtn.forEach((elem, index) => {
+    elem.addEventListener("click", function () {
+      subMenu[index].classList.toggle("active");
+      menuChevron[index].classList.toggle("active");
+    });
+  });
 }
 
 // slider with opacity
@@ -223,13 +250,92 @@ likes.forEach((elem, index) => {
 });
 
 // swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4.5,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  mousewheel: true,
-  keyboard: true,
+let swipersCount = document.querySelectorAll(".productSlider");
+
+for (index in swipersCount) {
+  var swiper = new Swiper(`.mySwiper${index}`, {
+    slidesPerView: 2,
+    spaceBetween: 5,
+    navigation: {
+      nextEl: `.swiper-button-next${index}`,
+      prevEl: `.swiper-button-prev${index}`,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+    keyboard: true,
+  });
+}
+
+// special offers timing
+let specialTimes = [
+  120, 24, 56, 93, 24, 90, 24, 24, 48, 24, 72, 48, 124, 100, 98, 48,
+];
+let specialMin = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let specialSec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let specialOfferTiming = document.querySelectorAll(".specialOfferTime");
+specialOfferTiming.forEach((elem, index) => {
+  callSpecialOffer();
+  function callSpecialOffer() {
+    if (specialSec[index] == 0) {
+      specialSec[index] = 59;
+      changeSOInnerHTML();
+
+      if (specialMin[index] == 0) {
+        specialMin[index] = 59;
+        changeSOInnerHTML();
+        if (specialTimes[index] == 0) {
+          changeSOInnerHTML();
+        } else {
+          specialTimes[index]--;
+          changeSOInnerHTML();
+        }
+      } else {
+        specialMin[index]--;
+        changeSOInnerHTML();
+      }
+    } else {
+      specialSec[index]--;
+      changeSOInnerHTML();
+    }
+    setTimeout(callSpecialOffer, 1000);
+  }
+  function changeSOInnerHTML() {
+    let timeHour = specialTimes[index];
+    let timeMin = specialMin[index];
+    let timeSec = specialSec[index];
+    if (timeHour < 10) {
+      timeHour = `0${timeHour}`;
+    }
+    if (timeMin < 10) {
+      timeMin = `0${timeMin}`;
+    }
+    if (timeSec < 10) {
+      timeSec = `0${timeSec}`;
+    }
+    elem.innerHTML = timeHour + ":" + timeMin + ":" + timeSec;
+  }
+});
+
+// back to top button
+let previousX = 0;
+let backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", function () {
+  if (document.querySelector("body").getBoundingClientRect().y < 0) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+backToTopBtn.addEventListener("click", function () {
+  scrollTo(0, 0);
 });
